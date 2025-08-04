d6f13ddc6a807f4c2571ee95a213a283877f886a
DrouetDanilo <ddrouet@espol.edu.ec>
2025-08-04 19:30:19 +0000

    guia23part1

--- backend_analytics_server/urls.py
+++ backend_analytics_server/urls.py
@@ -17,9 +17,12 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
+from dashboard import views as dashboard_view
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path('', include('dashboard.urls')),
+    path('dashboard/', include('dashboard.urls')),
+        path('', dashboard_view.index , name = "home"),
+
     
 ]
