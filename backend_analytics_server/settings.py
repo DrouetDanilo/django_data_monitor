4cd710c57d923c82548b1441aace644bc1cfc440
DrouetDanilo <ddrouet@espol.edu.ec>
2025-08-06 04:44:25 +0000

    guia23part3

--- backend_analytics_server/settings.py
+++ backend_analytics_server/settings.py
@@ -14,6 +14,7 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+API_URL = 'https://jsonplaceholder.typicode.com/posts'
 
 
 # Quick-start development settings - unsuitable for production
